ec9fd38c2cb521b7036b35019a2e52fc5b5fcc28
hiren <hirenpopat67@gmail.com>
2024-04-25 00:04:35 +0530

    Invoice template ready

--- app/routes/view_invoice.py
+++ app/routes/view_invoice.py
@@ -1,23 +1,33 @@
 from flask import Blueprint,render_template,redirect,current_app,request,flash,jsonify
 from flask_login import login_required
 import pdfkit
+import platform
 
 view_invoice_blueprint = Blueprint('view_invoice_blueprint', __name__)
 
 
 @view_invoice_blueprint.route('/view_invoice')
-# @login_required
+@login_required
 def view_invoice():
     try:
         options = {
             'page-size': 'A4',
         }
 
-        configuration = pdfkit.configuration(wkhtmltopdf='C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
+        if platform.system() == 'Windows':
+            configuration = pdfkit.configuration(
+                wkhtmltopdf='C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe')
 
-        css = 'css.css'
+        elif platform.system() == 'Linux':
+            configuration = pdfkit.configuration(
+                wkhtmltopdf='/usr/bin/wkhtmltopdf')
+
+        else:
+            current_app.logger.warning('Unknown operating system detected for wkhtmltopdf')
+            flash('Unknown operating system detected for wkhtmltopdf','error')
+            return redirect('500.html'), 500
        
-        pdfkit.from_file('test.html', 'out.pdf',options=options,configuration=configuration,css=css)
+        pdfkit.from_file('app/templates/view_invoice.html', 'out.pdf',options=options,configuration=configuration)
         return render_template('view_invoice.html')
 
     except Exception as e:
