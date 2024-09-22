e31fe58b10ffc3fa96aee33dd71120c71b0c9469
Hiren <hirenpopat67@gmail.com>
2024-09-22 20:39:50 +0530

    Added pagination in invoices page

--- app/routes/all_invoices.py
+++ app/routes/all_invoices.py
@@ -12,9 +12,15 @@ def all_invoices():
     try:
         data = []
 
-        fetch_all_invoices = Invoices.query.order_by(Invoices.invoice_no.desc()).all()
+        page = request.args.get('page', default=1, type=int)
+        per_page = request.args.get('per_page', default=25, type=int)
 
-        for fai in fetch_all_invoices:
+        if per_page > 100:
+            per_page = 100
+
+        fetch_all_invoices = Invoices.query.order_by(Invoices.invoice_no.desc()).paginate(page=page, per_page=per_page)
+
+        for fai in fetch_all_invoices.items:
             new_data = {}
             for column in fai.__table__.columns:
 
@@ -25,14 +31,32 @@ def all_invoices():
 
 
                 new_data[column.name] = column_value
+            
+            invoice_json = getattr(fai, 'invoice_json',{})
+
+            if invoice_json:
+                new_data['invoice_json'] = json.loads(invoice_json)
+            else:
+                new_data['invoice_json'] = {}
                 
-            new_data['invoice_json'] = json.loads((getattr(fai, 'invoice_json')))
-            invoice_date = fai.invoice_date
-            new_data['invoice_date'] = f"{invoice_date.day}/{invoice_date.month}/{invoice_date.year}"
+            invoice_date = getattr(fai, 'invoice_date',None)
+            if invoice_date:
+                new_data['invoice_date'] = f"{invoice_date.day}/{invoice_date.month}/{invoice_date.year}"
+            else:
+                new_data['invoice_date'] = None
 
             data.append(new_data)
         
-        return render_template('all_invoices.html',data=data)
+        context = {
+            "data_items":data,
+            "pagination":fetch_all_invoices,
+            "page":page,
+            "per_page":per_page,
+            "total_pages":fetch_all_invoices.pages,
+            "total_records":fetch_all_invoices.total,
+        }
+        
+        return render_template('all_invoices.html',context=context)
 
     except Exception as e:
         current_app.logger.error(f"{str(e)} WHICH_API = {request.path}", exc_info=True)
